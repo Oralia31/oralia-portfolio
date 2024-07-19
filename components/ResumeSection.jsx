3575da3a9c8aa0dd75efdb6a3a4f512ede18b1be
Oralia Rivera <oralia_rivera31@hotmail.com>
2024-07-19 15:07:56 -0600

    deleted changes

--- components/ResumeSection.jsx
+++ components/ResumeSection.jsx
@@ -38,7 +38,7 @@ const ResumeSection = () => {
             >
               <div className="flex flex-col gap-[30px]">
                 <h3 className="text-4xl font-bold">{ABOUT.title}</h3>
-                <p className="w-auto text-white/60 mx-auto xl:mx-0 text-left">
+                 <p className="w-auto text-white/60 mx-auto xl:mx-0 text-left">
                   {ABOUT.description}
                 </p>
                 <p className="w-auto text-white/60 mx-auto xl:mx-0 text-left">
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig = {
-    output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/oralia-portfolio/' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/oralia-portfolio' : '',
-    images: {
-      unoptimized: true
-    },
-  };
-  
-  export default nextConfig;
+export default nextConfig;
